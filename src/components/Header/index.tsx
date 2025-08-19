b1fbc6e6733751c616cf64e48ba383a7d24b9d32
Luan Antony <fplpgluan@gmail.com>
2025-08-19 16:55:45 -0300

    configurando tailwind

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+    return (
+        <header>
+            <nav>
+                <li><a href="#">Habilidades</a></li>
+                <li><a href="#">Projetos</a></li>
+                <li><a href="#">Contato</a></li>
+            </nav>
+        </header>
+    )
+}
